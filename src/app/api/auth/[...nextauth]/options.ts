0f8d7cbe0c72bd13bb89f9771189ddb2d98abbce
Alex <acastro166@gmail.com>
2024-04-01 13:26:57 -0700

    fixed import locations to be in line with next.js updates

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,16 +1,33 @@
-import type { NextAuthOptions } from 'next-auth'
-import { CredentialsProvider } from 'next-auth/providers/credentials'
-import Github from 'next-auth/providers/github'
-import { prisma } from "../../prismaClient.js";
-import PrismaAdapter from '@auth/prisma-adapter'
+import NextAuth from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GithubProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from "../../prismaClient";
+import { NextApiRequest } from 'next';
 
-export const oprtions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
+const prismaAdapter = PrismaAdapter(prisma)
+
+export const authOptions = NextAuth({
+    adapter: prismaAdapter,
     providers: [
-        Github({
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                username: { label: "email", type: "text", placeholder: "" },
+                password: {  label: "password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                return null
+            }
+        }),
+        GithubProvider({
             clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_ID as string
+            clientSecret: process.env.GITHUB_SECRET as string
         }),
     ],
-
-}
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: process.env.NODE_ENV === "development"
+})
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
+import { AuthOptions } from '../[...nextauth]/options';
 
-const handler = NextAuth();
+const handler = NextAuth(AuthOptions);
 
 export { handler as GET, handler as POST }
