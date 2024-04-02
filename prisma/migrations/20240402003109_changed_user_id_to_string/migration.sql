e704323d4617bdfb840485ae47d8ca448b44aa20
Alex <acastro166@gmail.com>
2024-04-01 17:34:16 -0700

    database changes * changed user related id's to strings and used cuid() instead of autoincrement() * checked that related type errors were resolved after db schema migration

---
+++ prisma/migrations/20240402003109_changed_user_id_to_string/migration.sql
@@ -0,0 +1,75 @@
+/*
+  Warnings:
+
+  - The primary key for the `Account` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Session` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `UserTask` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Account" DROP CONSTRAINT "Account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Project" DROP CONSTRAINT "Project_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ProjectTask" DROP CONSTRAINT "ProjectTask_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "UserTask" DROP CONSTRAINT "UserTask_authorId_fkey";
+
+-- AlterTable
+ALTER TABLE "Account" DROP CONSTRAINT "Account_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Account_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Account_id_seq";
+
+-- AlterTable
+ALTER TABLE "Project" ALTER COLUMN "authorId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "ProjectTask" ALTER COLUMN "authorId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Session" DROP CONSTRAINT "Session_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Session_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Session_id_seq";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AlterTable
+ALTER TABLE "UserTask" DROP CONSTRAINT "UserTask_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "authorId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "UserTask_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "UserTask_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserTask" ADD CONSTRAINT "UserTask_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProjectTask" ADD CONSTRAINT "ProjectTask_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,12 @@
-import NextAuth, { SessionStrategy } from 'next-auth'
+import NextAuth, { Session, SessionStrategy } from 'next-auth'
+import { JWT } from 'next-auth/jwt';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { prisma } from "../../prismaClient";
+import { User } from '@prisma/client';
 
+const bcrypt = require('bcrypt')
 const prismaAdapter = PrismaAdapter(prisma)
 
 export const authOptions = {
@@ -12,16 +15,24 @@ export const authOptions = {
         CredentialsProvider({
             name: 'credentials',
             credentials: {
-                username: { label: "email", type: "text", placeholder: "" },
-                password: {  label: "password", type: "password" }
+                username: { label: "email", type: "text", placeholder: "username" },
+                password: {  label: "password", type: "password" },
             },
-            async authorize(credentials, req) {
-                const user = await prisma.user.findUnique({where: {email: credentials?.username}})
-                if (!user) {
-                    return null;
-                }
-                const { email, hashedPassword } = user;
-                return null
+            async authorize(credentials): Promise<User | null> {
+                if (!credentials?.username || !credentials?.password) return null;
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials?.username,
+                    }
+                });
+
+                if (!user) return null;
+                
+                const passwordMatch = await bcrypt.compare(credentials.password, user.hashedPassword)
+                if (!passwordMatch) return null;
+
+                return user
             }
         }),
         GithubProvider({
@@ -29,6 +40,18 @@ export const authOptions = {
             clientSecret: process.env.GITHUB_SECRET as string
         }),
     ],
+    // callbacks: {
+    //     session({ session, token }: {session: Session; token: SessionToken }) {
+    //         session.user.id = token.id;
+    //         return session;
+    //     },
+    //     jwt({ token, account, user }: JWT) {
+    //         if (account) {
+    //             token.accessToken = account.access_token;
+    //             token.id = user.id;
+    //         }
+    //     }
+    // },
     session: {
         strategy: "jwt" as SessionStrategy,
     },
