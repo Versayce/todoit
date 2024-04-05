aac12bd1161b3ba716fac589245e1b7e2cac9741
Alex <acastro166@gmail.com>
2024-04-05 15:08:53 -0700

    added delete route for User table

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../prismaClient";
+import bcrypt from 'bcrypt'
 
 export async function POST(req: Request) {
   try {
@@ -8,20 +9,30 @@ export async function POST(req: Request) {
 
     if (!email || !username) return new Response('No data provided for one or more fields', { status: 400 })
 
-    const thisUser = await prisma.user.findFirst({where: {email: email}})
+    const thisUser = await prisma.user.findFirst({ where: { email: email } })
     const allUsers = await prisma.user.findMany();
 
-    return new Response(JSON.stringify({ thisUser }), { status: 200 });
+    return new Response(JSON.stringify({ allUsers }), { status: 200 });
   } catch (error) {
+
     return new Response(`${ error }`, { status: 500 })
   }
 }
 
-
 export async function DELETE(req: Request) {
   try {
+    const data = await req.json();
+    const { email, username, password } = data;
+    
+    const thisUser = await prisma.user.findFirst({ where: { email: email } })
+    if (thisUser) {
+      const deletedUser = await prisma.user.delete({where: {email: thisUser?.email}});
+      return new Response(`User: ${deletedUser.email} deleted`, { status: 200 });
+    }
 
+    return new Response('Given user not found in database', { status: 400 })
   } catch(error) {
+
     return new Response(`${ error }`, { status: 500 })
   }
 }
