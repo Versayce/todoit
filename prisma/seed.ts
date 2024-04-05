838810dc1f88d98a6cfb043f3060179688dc7038
Alex <acastro166@gmail.com>
2024-04-05 16:17:35 -0700

    added seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,23 @@
 import { PrismaClient } from "@prisma/client/extension";
+import bcrypt from "bcrypt"
 
 const prisma = new PrismaClient()
 
 async function seedDatabase() {
-    const testUser = await prisma.user.
+    const password = "1234"
+    const hashedPass = bcrypt.hash(password, 14)
+    const testUser = await prisma.user.upsert({
+        where: { email: 'user1@test.io' },
+        update: {},
+        create: {
+            email: 'user1@test.io',
+            username: 'user1',
+            hashedPassword: hashedPass, 
+            tasks: {
+                create: {
+                    
+                }
+            }
+        }
+    })
 }
