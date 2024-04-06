faae262fededc58a7ee15039ba4d50b21cb7dc40
Alex <acastro166@gmail.com>
2024-04-05 21:20:35 -0700

    reset migrations

--- prisma/migrations/20240406002012_ondelete_author/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "UserTask" DROP CONSTRAINT "UserTask_authorId_fkey";
-
--- AddForeignKey
-ALTER TABLE "UserTask" ADD CONSTRAINT "UserTask_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
