d5727d8636f1201dd819626464e5782241583210
Alex <acastro166@gmail.com>
2024-04-07 00:22:21 -0700

    added project route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,7 +1,11 @@
+import { getServerSession } from "next-auth";
 import { prisma } from "../prismaClient";
 import bcrypt from 'bcrypt'
+import { authOptions } from "../auth/[...nextauth]/options";
 
 export async function GET() {
+  const session = await getServerSession(authOptions);
+  if (!session) return new Response('Unauthorized, please sign in.', { status: 401 })
   try {
     const allUsers = await prisma.user.findMany({
       select: {
@@ -34,24 +38,24 @@ export async function GET() {
 }
 
 export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session) return new Response('Unauthorized, please sign in.', { status: 401 })
   try {
     const data = await req.json();
     const { email, username, id } = data;
-    console.log('req data: ', data);
 
-    if (!email || !username || !id) return new Response('No data provided for one or more fields', { status: 400 })
+    if (!email || !id) return new Response('No data provided for one or more fields.', { status: 400 })
 
     const thisUser = await prisma.user.findFirst({
       where: {
         OR: [
           { email: email },
-          { username: username },
           { id: id }
         ]
       }
     })
 
-    if (!thisUser) return new Response('User not found', { status: 404 })
+    if (!thisUser) return new Response('User not found.', { status: 404 })
 
     return new Response(JSON.stringify({ thisUser }), { status: 200 });
   } catch (error) {
@@ -61,6 +65,8 @@ export async function POST(req: Request) {
 }
 
 export async function DELETE(req: Request) {
+  const session = await getServerSession(authOptions);
+  if (!session) return new Response('Unauthorized, please sign in.', { status: 401 })
   try {
     const data = await req.json();
     const { email, password } = data;
@@ -68,15 +74,17 @@ export async function DELETE(req: Request) {
     if (!email || !password) return new Response('The required credentials have not been given', { status: 500 })
     
     const existingUser = await prisma.user.findUnique({ where: { email: email } })
+
     if (existingUser?.hashedPassword) {
       const passwordMatch = await bcrypt.compare(password, existingUser.hashedPassword)
       if (passwordMatch) {
         const deletedUser = await prisma.user.delete({ where: { email: existingUser?.email }, select: { id: true, email: true } });
-        return new Response(`User: ${ JSON.stringify(deletedUser) } deleted`, { status: 200 });
+        
+        return new Response(`User: ${ JSON.stringify(deletedUser) } deleted.`, { status: 200 });
       }
     }
 
-    return new Response('Given user not found in database', { status: 400 })
+    return new Response('Given user not found in database.', { status: 400 })
   } catch(error) {
 
     return new Response(`${ error }`, { status: 500 })
