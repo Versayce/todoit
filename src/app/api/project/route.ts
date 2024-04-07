e52179feae0fac32cb2da71af2cca0364251d2f3
Alex <acastro166@gmail.com>
2024-04-07 00:48:31 -0700

    returning Response.json instead of new Response

--- src/app/api/project/route.ts
+++ src/app/api/project/route.ts
@@ -1,17 +1,16 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/options";
 import { prisma } from "../prismaClient";
-import { NextResponse } from "next/server";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
   const session = await getServerSession(authOptions);
   if (!session)
-    return NextResponse.json("Unauthorized, please sign in.", { status: 401 });
+    return Response.json("Unauthorized, please sign in.", { status: 401 });
   try {
     const data = await req.json();
     const { id } = data;
 
-    if (!id) return NextResponse.json("Required data missing.", { status: 400 });
+    if (!id) return Response.json("Required data missing.", { status: 400 });
 
     const userProjects = await prisma.project.findMany({
       where: { authorId: id },
@@ -30,10 +29,12 @@ export async function POST(req: Request, res: Response) {
       }
     });
 
-    if (!userProjects) return NextResponse.json("Projects not found.", { status: 404 });
+    if (!userProjects) return Response.json("Projects not found.", { status: 404 });
 
-    return NextResponse.json(({ userProjects }), { status: 200 });
+    console.log(userProjects)
+
+    return Response.json(({ userProjects }), { status: 200 });
   } catch (error) {
-    return NextResponse.json(`${error}`, { status: 500 });
+    return Response.json(`${error}`, { status: 500 });
   }
 }
