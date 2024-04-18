adabae95c3a9f8d8b451152dae65591f54c39b8e
Alex <acastro166@gmail.com>
2024-04-18 13:59:43 -0700

    moved suspense fallback

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -71,8 +71,7 @@ const SignInPage = (): React.ReactNode => {
     }, [searchParams]);
 
     return (
-        <Suspense fallback={<Loading/>}>
-            <div className="flex flex-col justify-center items-center h-screen bg-gray-200">
+        <div className="flex flex-col justify-center items-center h-screen bg-gray-200">
                 <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                     <div className="mb-4">
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
@@ -85,10 +84,12 @@ const SignInPage = (): React.ReactNode => {
                             Password:
                         </label>
                         <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" value={password} onChange={e => setPassword(e.target.value)} required />
-                        {error ? 
-                            <p className="flex flex-col items-center w-full h-3 text-red-600">{error}</p> 
-                            : <p className="flex flex-col items-center w-full h-3"></p>
-                        }
+                        <Suspense fallback={<Loading/>}>
+                            {error ? 
+                                <p className="flex flex-col items-center w-full h-3 text-red-600">{error}</p> 
+                                : <p className="flex flex-col items-center w-full h-3"></p>
+                            }
+                        </Suspense>
                     </div>
                     <div className="flex items-center justify-center">
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
@@ -109,7 +110,6 @@ const SignInPage = (): React.ReactNode => {
                     Sign In with Google
                 </button>
             </div>
-        </Suspense>
     );
 };
 
