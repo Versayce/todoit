9cb83e80e0db6bf0d42b272bb95c1a8cc0775157
Alex <acastro166@gmail.com>
2024-04-22 12:15:26 -0700

    created utils folder * moved todoitFetch function into api/utils

--- src/lib/features/project/requests.ts
+++ src/lib/features/project/requests.ts
@@ -1,3 +1,5 @@
+import todoitFetch from "@/app/api/utils/fetch";
+
 type ProjectApiResponse = {
     id: string,
     title: string,
@@ -10,29 +12,6 @@ type ProjectApiResponse = {
         priority?: string,
         completionStatus: boolean,
     }[]
-}
-
-export async function todoitFetch(endpoint: string, options?: RequestInit): Promise<Response> {
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`,
-        ...options?.headers,
-    };
-
-    const finalOptions = {
-        ...options,
-        headers,
-    };
-
-    return fetch(`${endpoint}`, finalOptions).then(async (response) => {
-        if (!response.ok) {
-            const error = await response.json();
-            throw new Error(error.message);
-        }
-        return response
-    }).catch((error) => {
-        throw error;
-    });
 }
 
 export async function getUserProjectsByUserId(id: string): Promise<ProjectApiResponse[]> {
